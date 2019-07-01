ce93bf25be9c1ddafbee3fd7871e145dec018174
Ramon Barajas <ramonb621@yahoo.com>
2019-07-01 14:23:07 -0700

    condensed crystal onclick events in game.js and created a temporary score keeper for click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,51 @@ $(document).ready(function() {
 
     var losses = "";
 
+    var score = "";
+
+    
+    $("#random-number").ready(function(){
+
+        mainRandom =Math.floor(Math.random(Math.min(19) * (Math.max(10))) * 100);
+
+        $("#random-number").text(mainRandom);
+        //WORKING
+    })
+
     $(".crystal").on("click", function(){
-        crystalRandom = Math.random(Math.min(19) * (Math.max(10))) * 100;
-            
+ 
+        crystalRandom =Math.floor(Math.random(Math.min(1) * (Math.max(12)))  * 10);
+        //MIGHT HAVE TO DISSECT TO MAKE RANDOM # THAT STAYS THE SAME EACH ROUND FOR EACH CRYSTAL
+        
+
+        updateScore();//TRIGGER EVENT TO UPDATE SCORE??
         console.log(crystalRandom);
-    });
-});
+    })
+    
+    // $("#crystal2").on("click", function(){
+
+    //     crystalRandom =Math.floor(Math.random(Math.min(1) * (Math.max(12))) * 10);
+    //     //MIGHT HAVE TO DISSECT TO MAKE RANDOM # THAT STAYS THE SAME EACH ROUND 
+    //     console.log(crystalRandom);
+    // })
+
+    // $("#crystal3").on("click", function(){
+
+    //     crystalRandom =Math.floor(Math.random(Math.min(1) * (Math.max(12))) * 10);
+    //     //MIGHT HAVE TO DISSECT TO MAKE RANDOM # THAT STAYS THE SAME EACH ROUND 
+    //     console.log(crystalRandom);
+    // })
+    // $("#crystal4").on("click", function(){
+
+    //     crystalRandom =Math.floor(Math.random(Math.min(1) * (Math.max(12))) * 10);
+    //     //MIGHT HAVE TO DISSECT TO MAKE RANDOM # THAT STAYS THE SAME EACH ROUND 
+    //     console.log(crystalRandom);
+    // })
+    
+    function updateScore(){
+
+    document.getElementById("score").innerText = crystalRandom += score;
+    // $("#score").trigger("click");
+    
+    }
+})    
